d4caa7eb950571515921ffe489125de649f70038
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-01-26 21:19:46 +0900

    add login user name

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+	console.log("log session");
 	console.log(req.session);
+	console.log("log user");
 	console.log(req.user);
   res.render('index', { title: 'JDMstudio', 
   						user : req.session.passport.user || {}
