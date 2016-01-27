dedaaa1f0eb2d6e6a0a0693b58af1e6d74761048
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-01-27 20:59:57 +0900

    add login users name

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,14 @@ router.get('/',ensureAuthenticated, function(req, res) {
   });
 });
 function ensureAuthenticated(req,res,next){
-	if(req.isAuthenticated()){
+	//if(req.isAuthenticated()){
 		console.log("log session");
 		console.log(req.session);
 		console.log("log user");
 		console.log(req.user);	
-		return next();
-	}
-	res.redirect('/');
+	//	return next();
+	//}
+	//res.redirect('/');
 }
 
 module.exports = router;
