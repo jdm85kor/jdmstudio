121218ef4244e2c252e3bf1a4fbdc3d5da319915
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-01-28 01:53:04 +0900

    add login users name

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'jdm',
-  resave: false,
+  resave: true,
   saveUninitialized: true,
   cookie: { secure: true }
 }));
