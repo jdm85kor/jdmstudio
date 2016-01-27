325dcc636b021a1f9e683ded9f375dc0aa495094
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-01-27 23:48:29 +0900

    add login users name

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users page. */
-router.get('/',ensureAuthenticated, function(req, res) {
+router.get('/',app.ensureAuthenticated, function(req, res) {
 	
   	res.render('users', { title: 'JDMstudio',
   						  user : req.user
