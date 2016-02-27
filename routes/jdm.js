fac299f951851a85e5d82bddc68852a5bda15fba
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-02-27 13:34:21 +0900

    add jdm

--- routes/jdm.js
+++ routes/jdm.js
@@ -1,1 +1,21 @@
-jdm.js
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+function ensureAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
+
+/* GET users page. */
+router.get('/',ensureAuthenticated, function(req, res) {
+//	console.log(req);
+  	res.render('jdm', { title: 'JDMstudio'
+  						, user : req.user
+  });
+});
+
+//
+module.exports = router;
