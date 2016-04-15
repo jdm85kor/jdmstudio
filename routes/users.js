8588b8d4aeb43dcc2eaf5a6280127e534486f701
Dongmin Jang <jdm85kor@gmail.com>
2016-04-15 07:40:03 +0000

    mongodb_4

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,23 @@ function ensureAuthenticated(req,res,next){
   res.redirect('/');
 }
 
+/* Insert username into DB  */
+var insertUsername = function(db,user,callback){
+  db.collection('db').insertOne({
+    "username": user
+    }, function(err,result){
+      console.log('Insert username that login this web');
+      callback();
+    });
+};
+
 /* GET users page. */
 router.get('/',ensureAuthenticated, function(req, res) {
 //	console.log(req);
   	res.render('users', { title: 'JDMstudio'
   						, user : req.user
   });
+  
 });
 
 //
