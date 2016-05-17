399df27b44a00320c10d8179ec3b7c95fcfc5614
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-05-18 02:08:14 +0900

    modify app.js for WechatStrategy

--- routes/auth_login.js
+++ routes/auth_login.js
@@ -28,7 +28,7 @@ router.get('/google/callback',
 }));
 
 /* wechat auth button */
-router.get('/wechat',passport.authenticate('wechat',options));
+router.get('/wechat',passport.authenticate('wechat'));
 
 /* wechat auth callback */
 router.get('/wechat/callback',
