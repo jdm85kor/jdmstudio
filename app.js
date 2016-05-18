7c8f5b643caf5388da8eb179062b32a2d24009ab
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-05-19 01:27:54 +0900

    re

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ var passport = require('passport')
   , GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.serializeUser(function(user,done){
+  console.log(user.displayName);
   done(null,user.displayName);
 });
 
