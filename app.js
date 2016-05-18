a9ff5ffd991c48c3e0c836bdd1175962f504c997
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-05-19 00:16:02 +0900

    soso

--- app.js
+++ app.js
@@ -45,10 +45,9 @@ passport.deserializeUser(function(user,done){
 passport.use(new FacebookStrategy({
   clientID:pkginfo.oauth.facebook.FACEBOOK_APP_ID,
   clientSecret:pkginfo.oauth.facebook.FACEBOOK_APP_SECRET,
-  callbackURL:pkginfo.oauth.facebook.callbackURL,
-  passReqToCallback : true
+  callbackURL:pkginfo.oauth.facebook.callbackURL
   },
-  function(req, accessToken, refreshToken, profile, done){
+  function(accessToken, refreshToken, profile, done){
     done(null,profile);
   }
 ));
