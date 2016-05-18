ce9520bba7ce1e8a18957d318c7bcde87203952d
JDM <jdm@twin-ui-MacBook-Pro.local>
2016-05-18 21:44:48 +0900

    modify app.js for WechatStrategy

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ passport.use(new WechatStrategy({
   appSecret:   pkginfo.oauth.wechat.client_secret,
   client:      {},
   callbackURL: pkginfo.oauth.wechat.callbackURL,
-  scope:       {},
+  scope:       {snsapi_userinfo},
   state:       {},
   passReqToCallback   : true
   },
