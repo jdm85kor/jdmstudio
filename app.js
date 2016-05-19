55abcf264319ea267d44775afdfcb67ab436d717
Dongmin Jang <jdm85kor@gmail.com>
2016-05-19 09:44:16 +0000

    passport

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use( express.static(path.join(__dirname, 'public')) );
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -100,7 +103,7 @@ console.log("passport.initialize()");
 app.use( passport.initialize() );
 console.log("passport.session()");
 app.use( passport.session() );
-app.use( express.static(path.join(__dirname, 'public')) );
+
 
 MongoClient.connect(dbUrl,function(err,db){
   console.log("Connected correctly to server.");
